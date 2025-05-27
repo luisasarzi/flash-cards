df96991dbe13d6f01d24b821f45378d9e9cad44b
luisasarzi <luisa.cordeiro@escola.pr.gov.br>
2025-05-27 19:23:58 +0000

    aula 10

--- perguntas.js
+++ perguntas.js
@@ -1,3 +1,23 @@
+
+criaCartao(
+    'Língua Portuguesa',
+    ' O que é figura de linguagem?',
+    'Figuras de linguagem são recursos utilizados para tornar a comunicação mais expressiva, criativa ou impactante.'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    '  Metáfora',
+    'Comparação implícita, sem o uso de "como".'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    '  Metonímia',
+    'Substituição de uma palavra por outra com relação de proximidade.'
+)
+
+
 criaCartao(
     'Língua Portuguesa',
     ' Hiperbole',
@@ -15,3 +35,4 @@ criaCartao(
     'Ironia',
     'Afirmação contrária ao que se quer dizer, com tom de crítica ou sarcasmo.'
 )
+
