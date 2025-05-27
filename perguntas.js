674e8bd04dcdaa1fd17dab8f9be5ebf0ed2850d3
luisasarzi <luisa.cordeiro@escola.pr.gov.br>
2025-05-27 18:43:01 +0000

    aula 6

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Língua Portuguesa',
+    ' Hiperbole',
+    'Exagero intencional para dar ênfase.'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Personificação (ou Prosopopeia)',
+    'Atribuição de características humanas a seres inanimados ou irracionais.'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Ironia',
+    'Afirmação contrária ao que se quer dizer, com tom de crítica ou sarcasmo.'
+)
